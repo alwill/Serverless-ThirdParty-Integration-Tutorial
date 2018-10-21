b71e450ae9e286ab0849e3d7308888a53b23cc77
Alex Will <alwill@Alexs-MacBook-Pro.local>
2018-10-21 08:09:30 -0500

    Blob upload function'

--- CustomerFeedbackUpload.cs
+++ CustomerFeedbackUpload.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -13,21 +14,13 @@ namespace AlexWill
     public static class CustomerFeedbackUpload
     {
         [FunctionName("CustomerFeedbackUpload")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "customerfeedback/{id}")] string body,
+            [Blob("customer-feedback/{id}", FileAccess.Write)] out string feedbackBlob, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            feedbackBlob = body;
+            return (ActionResult)new OkObjectResult($"Success");
         }
     }
 }
